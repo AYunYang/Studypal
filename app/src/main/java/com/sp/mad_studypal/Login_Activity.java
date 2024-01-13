b023b4012f8c42a712c6467d67ab50281368dfd9
ChaiJunSheng <junsheng.22@ichat.sp.edu.sg>
2024-01-14 01:09:25 +0800

    Input Validation finished
    14-1 / 1.09am

--- app/src/main/java/com/sp/mad_studypal/Login_Activity.java
+++ app/src/main/java/com/sp/mad_studypal/Login_Activity.java
@@ -5,10 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Login_Activity extends AppCompatActivity {
+    private TextInputLayout login_username_layout;
+    private TextInputLayout login_password_layout;
+    private TextInputEditText login_username;
+    private TextInputEditText login_password;
+    private Button button_login;
     private TextView button_switch_to_signup;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +29,49 @@ public class Login_Activity extends AppCompatActivity {
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow); //Set the icon to back_arrow
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        login_username_layout = findViewById(R.id.id_login_username_layout);
+        login_password_layout = findViewById(R.id.id_login_password_layout);
+        login_username = findViewById(R.id.id_login_username);
+        login_password = findViewById(R.id.id_login_password);
+
+        button_login = findViewById(R.id.button_login_id);
+        button_login.setOnClickListener(login);
+
         button_switch_to_signup = findViewById(R.id.button_switch4_id);
-        button_switch_to_signup.setOnClickListener(switch_login);
+        button_switch_to_signup.setOnClickListener(switch_to_signup);
     }
 
-    private View.OnClickListener switch_login = new View.OnClickListener() {     //Button, switch to signup page
+    private View.OnClickListener switch_to_signup = new View.OnClickListener() {     //Button, switch to signup page
         @Override
         public void onClick(View v) {
             startActivity(new Intent(Login_Activity.this, StartPage_Activity.class));
         }
     };
+
+    private View.OnClickListener login = new View.OnClickListener() {    //Login button
+        @Override
+        public void onClick(View v) {
+            String usernameStr = login_username.getText().toString().trim();
+            String passwordStr = login_password.getText().toString().trim();
+
+            if (usernameStr.isEmpty()){                                 //If username is empty
+                login_username_layout.setHelperText("Username Required !");
+                login_password_layout.setHelperText(" ");
+                return;
+            }
+            if (passwordStr.isEmpty()){                                 //If Password is empty
+                login_username_layout.setHelperText(" ");
+                login_password_layout.setHelperText("Password Required !");
+                return;
+            }
+
+            else {
+                login_username_layout.setHelperText(" ");
+                login_password_layout.setHelperText(" ");
+                Toast.makeText(getApplicationContext(),"Good",Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+
 }
--- app/src/main/java/com/sp/mad_studypal/Signup_Activity.java
+++ app/src/main/java/com/sp/mad_studypal/Signup_Activity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.firebase.Firebase;
-import com.google.firebase.FirebaseApiNotAvailableException;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class Signup_Activity extends AppCompatActivity {
+    private TextInputLayout signup_username_layout;
+    private TextInputLayout signup_email_layout;
+    private TextInputLayout signup_password_layout;
+    private TextInputLayout signup_cpassword_layout;
+    private TextInputEditText signup_username;
+    private TextInputEditText signup_email;
+    private TextInputEditText signup_cpassword;
+    private TextInputEditText signup_password;
+    private Button button_signup;
     private TextView button_switch_to_login;
 
     @Override
@@ -18,9 +29,21 @@ public class Signup_Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-
         getSupportActionBar().setHomeAsUpIndicator(R.drawable.back_arrow); //Set the icon to back_arrow
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        signup_username_layout = findViewById(R.id.id_signup_username_layout);
+        signup_email_layout = findViewById(R.id.id_signup_email_layout);
+        signup_password_layout = findViewById(R.id.id_signup_password_layout);
+        signup_cpassword_layout = findViewById(R.id.id_signup_cpassword_layout);
+
+        signup_username = findViewById(R.id.id_signup_username);
+        signup_email = findViewById(R.id.id_signup_email);
+        signup_password = findViewById(R.id.id_signup_password);
+        signup_cpassword = findViewById(R.id.id_signup_cpassword);
+
+        button_signup = findViewById(R.id.button_signup_id);
+        button_signup.setOnClickListener(signup);
 
         button_switch_to_login = findViewById(R.id.button_switch4_id);
         button_switch_to_login.setOnClickListener(switch_login);
@@ -30,6 +53,60 @@ public class Signup_Activity extends AppCompatActivity {
         @Override
         public void onClick(View v) {
             startActivity(new Intent(Signup_Activity.this, Login_Activity.class));
+        }
+    };
+
+    private View.OnClickListener signup = new View.OnClickListener() {     // Signup button
+        @Override
+        public void onClick(View v) {
+
+            String usernameStr = signup_username.getText().toString().trim();
+            String emailStr = signup_email.getText().toString().trim();
+            String passwordStr = signup_password.getText().toString().trim();
+            String cpasswordStr = signup_cpassword.getText().toString().trim();
+
+
+            if (usernameStr.isEmpty()){                                 //If username is empty
+                signup_username_layout.setHelperText("Username Required !");
+                signup_email_layout.setHelperText(" ");
+                signup_password_layout.setHelperText(" ");
+                signup_cpassword_layout.setHelperText(" ");
+                return;
+            }
+            if (emailStr.isEmpty()){                                 //If Email is empty
+                signup_username_layout.setHelperText(" ");
+                signup_email_layout.setHelperText("Email Required !");
+                signup_password_layout.setHelperText(" ");
+                signup_cpassword_layout.setHelperText(" ");
+                return;
+            }
+
+            if (passwordStr.isEmpty()){                                 //If Password is empty
+                signup_username_layout.setHelperText(" ");
+                signup_email_layout.setHelperText(" ");
+                signup_password_layout.setHelperText("Password Required !");
+                signup_cpassword_layout.setHelperText(" ");
+                return;
+            }
+            if (!cpasswordStr.equals(passwordStr)){                                 //If Confirm Password is not equal to Password
+                signup_username_layout.setHelperText(" ");
+                signup_email_layout.setHelperText(" ");
+                signup_password_layout.setHelperText(" ");
+                signup_cpassword_layout.setHelperText("Password must be the same");
+                return;
+            }
+
+            else {
+                signup_username_layout.setHelperText(" ");
+                signup_email_layout.setHelperText(" ");
+                signup_password_layout.setHelperText(" ");
+                signup_cpassword_layout.setHelperText(" ");
+                Toast.makeText(getApplicationContext(),"Good",Toast.LENGTH_SHORT).show();
+            }
+
+
+
+
         }
     };
 
