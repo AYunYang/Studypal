12623204c8e32f03f4f0cb78c420d4fff7711c66
ChaiJunSheng <junsheng.22@ichat.sp.edu.sg>
2024-02-01 15:32:53 +0800

    Reservation + Saved Back Button
    1-2 / 3.30pm

--- app/src/main/java/com/sp/mad_studypal/Reservation_Activity.java
+++ app/src/main/java/com/sp/mad_studypal/Reservation_Activity.java
@@ -1,16 +1,20 @@
 package com.sp.mad_studypal;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 
 public class Reservation_Activity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reservation);
-    }
 
+        toolbar = findViewById(R.id.toolbar_profile);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
 
 }
--- app/src/main/java/com/sp/mad_studypal/Saved_Activity.java
+++ app/src/main/java/com/sp/mad_studypal/Saved_Activity.java
@@ -1,14 +1,20 @@
 package com.sp.mad_studypal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class Saved_Activity extends AppCompatActivity {
-
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_saved);
+
+        toolbar = findViewById(R.id.toolbar_profile);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
     }
 }
