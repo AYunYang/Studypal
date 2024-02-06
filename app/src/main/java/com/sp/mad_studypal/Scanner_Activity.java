ca1e5a735d887307198e9f48119bb69b5bd5e122
AYunYang <130944403+AYunYang@users.noreply.github.com>
2024-02-06 23:59:31 +0800

    added cammera and info page

--- app/src/main/java/com/sp/mad_studypal/Scanner_Activity.java
+++ app/src/main/java/com/sp/mad_studypal/Scanner_Activity.java
@@ -1,19 +1,29 @@
 package com.sp.mad_studypal;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
 
 public class Scanner_Activity extends AppCompatActivity {
     private BottomNavigationView bottom_menu;
     private String current_email;
+
+    private Button btn_scan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,12 +37,43 @@ public class Scanner_Activity extends AppCompatActivity {
 
         holder object = new holder(getApplicationContext());
         current_email = object.getVariable();
+
+        btn_scan = findViewById(R.id.btn_scan);
+        btn_scan.setOnClickListener(v -> {scanCode();});
     }
 
     protected void onStart(){
         bottom_menu.setSelectedItemId(R.id.bottom_item_scanner);   //Default input
         super.onStart();
     }
+
+
+
+    private void scanCode(){
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Scan a barcode");
+        options.setBeepEnabled(true);
+        options.setBarcodeImageEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(capture_activity.class);
+        barLauncher.launch(options);
+
+    }
+
+    //launch the camera scanning
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+        if(result.getContents() !=null){
+            AlertDialog.Builder builder = new AlertDialog.Builder(Scanner_Activity.this);
+            builder.setTitle("Result");
+            builder.setMessage(result.getContents());
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            }).show();
+        }
+    });
 
     NavigationBarView.OnItemSelectedListener bottom_menu_select =  new NavigationBarView.OnItemSelectedListener() {      //Bottom Menu selected
         @Override
---
+++ app/src/main/java/com/sp/mad_studypal/capture_activity.java
@@ -0,0 +1,7 @@
+package com.sp.mad_studypal;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class capture_activity extends CaptureActivity {
+    //empty
+}
